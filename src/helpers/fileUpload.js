ed0a9b7cc865e8393244240e5c8a27f329aa8a78
Cristian López <cristianlopezr.931@gmail.com>
2023-03-25 14:12:44 -0300

    environment variables added

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -1,26 +1,26 @@
 export const fileUpload = async file => {
-	/* if (!file) throw new Error('No hay ningún archivo para subir'); */
-	if(!file) return null;
+    /* if (!file) throw new Error('No hay ningún archivo para subir'); */
+    if (!file) return null;
 
-	const cloudUrl = 'https://api.cloudinary.com/v1_1/dmpsvqars/upload';
-	const formData = new FormData();
-	formData.append('upload_preset', 'react-journal');
-	formData.append('file', file);
+    const cloudUrl = env.VITE_CLOUD_URL;
+    const formData = new FormData();
+    formData.append('upload_preset', 'react-journal');
+    formData.append('file', file);
 
-	try {
-		const resp = await fetch(cloudUrl, {
-			method: 'POST',
-			body: formData,
-		});
+    try {
+        const resp = await fetch(cloudUrl, {
+            method: 'POST',
+            body: formData,
+        });
 
-		if (!resp.ok) throw new Error('No se pudo subir la imagen');
+        if (!resp.ok) throw new Error('No se pudo subir la imagen');
 
-		const cloudResp = await resp.json();
+        const cloudResp = await resp.json();
 
-		return cloudResp.secure_url;
-	} catch (error) {
-		/* console.log(error);
+        return cloudResp.secure_url;
+    } catch (error) {
+        /* console.log(error);
 		throw new Error(error.message); */
-		return null;
-	}
+        return null;
+    }
 };
